a9ecd60631c5272325e35c2a1286def103390e90
CherryB331 <brianna.cherry@salem.edu>
2026-03-18 18:06:10 -0400

    v

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ answerButtons.addEventListener("click", function(e) {
     const answer = this.dataset.answer;
     console.log("You clicked answer: " + answer);
 }
-const blueScore = 0;
-const brownScore = 0;
-const greenScore = 0;
-const redScore = 0;
+const blueScore = (0);
+const brownScore = (0);
+const greenScore = (0);
+const redScore = (0);
