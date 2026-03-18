d651bec935bbcfb128a16fd99f659628efc54cb8
CherryB331 <brianna.cherry@salem.edu>
2026-03-18 18:08:11 -0400

    h

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 console.log("script.js connected!");
 const answerButtons = document.querySelectorAll(".answer-btn");
-answerButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const answer = button.getAttribute("data-answer");
-    console.log(`You selected answer ${answer}`);
-    // Here you can add code to update the scores based on the selected answer
-  }
 const blueScore = (0);
 const brownScore = (0);
 const greenScore = (0);
