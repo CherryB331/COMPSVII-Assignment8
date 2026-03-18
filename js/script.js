fdb13a35e19e4d7c80680f23259ce2c4f5ee19d0
CherryB331 <brianna.cherry@salem.edu>
2026-03-18 18:32:34 -0400

    t

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@ const blue = document.getElementById("blue");
 const brown = document.getElementById("brown");
 const green = document.getElementById("green");
 const red = document.getElementById("red");
-blue.addEventListener("click",blueScoring ) => {
+blue.addEventListener("click",blueScoring ) {
     function blueScoring(userScore, blueScore) {userScore + blueScore};
 };
-brown.addEventListener("click",brownScoring ) => {
+brown.addEventListener("click",brownScoring )  {
     function brownScoring(userScore, brownScore) {userScore + brownScore};
 };
-green.addEventListener("click",greenScoring ) => {
+green.addEventListener("click",greenScoring ) {
     function greenScoring(userScore, greenScore) {userScore + greenScore};
 };
-red.addEventListener("click",redScoring) => {
+red.addEventListener("click",redScoring) {
     function redScoring(userScore, redScore) {userScore + redScore};
 };
 const userScore = (0);
