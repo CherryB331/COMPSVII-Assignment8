0949c90429b7bedfcb126ae3fed8e3ca2849d95b
CherryB331 <brianna.cherry@salem.edu>
2026-03-18 18:34:49 -0400

    r

--- js/script.js
+++ js/script.js
@@ -5,18 +5,19 @@ const blue = document.getElementById("blue");
 const brown = document.getElementById("brown");
 const green = document.getElementById("green");
 const red = document.getElementById("red");
-blue.addEventListener("click",blueScoring ) {
-    function blueScoring(userScore, blueScore) {userScore + blueScore};
-};
-brown.addEventListener("click",brownScoring )  {
-    function brownScoring(userScore, brownScore) {userScore + brownScore};
-};
-green.addEventListener("click",greenScoring ) {
-    function greenScoring(userScore, greenScore) {userScore + greenScore};
-};
-red.addEventListener("click",redScoring) {
+blue.addEventListener("click", blueScoring);
+function blueScoring(userScore, blueScore) { return userScore + blueScore; }
+
+brown.addEventListener("click", brownScoring);
+function brownScoring(userScore, brownScore) { return userScore + brownScore; }
+
+green.addEventListener("click", greenScoring);
+function greenScoring(userScore, greenScore) { return userScore + greenScore; }
+
+red.addEventListener("click", redScoring);
+function redScoring(userScore, redScore) { return userScore + redScore; }
     function redScoring(userScore, redScore) {userScore + redScore};
-};
+
 const userScore = (0);
 const blueScore = (1);
 const brownScore = (2);
