00b4332965103e0c84201b1a146254e9951b097c
CherryB331 <brianna.cherry@salem.edu>
2026-03-18 18:17:49 -0400

    h

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 console.log("script.js connected!");
 const answerButtons = document.querySelectorAll(".answer-btn");
 const questionContainer = document.getElementById("question-container");
-answerButtons.addEventListener("click", handleAnswerClick);
+answerButtons.addEventListener("click");
 const userScore = (0);
 const blueScore = (1);
 const brownScore = (2);
