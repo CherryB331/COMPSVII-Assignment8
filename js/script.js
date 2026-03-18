479f9c576b8707de564bd41e7b48d205d02ddc83
CherryB331 <brianna.cherry@salem.edu>
2026-03-18 18:30:54 -0400

    work please

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 console.log("script.js connected!");
 const answerButtons = document.querySelectorAll(".answer-btn");
 const questionContainer = document.getElementById("question-container");
-answerButtons.addEventListener ("click", answerClicked);
-function answerClicked() {console.log("Answer button clicked!");}
+const blue = document.getElementById("blue");
+const brown = document.getElementById("brown");
+const green = document.getElementById("green");
+const red = document.getElementById("red");
+blue.addEventListener("click",blueScoring () => {
+    function blueScoring(userScore, blueScore) {userScore + blueScore};
+};
+brown.addEventListener("click",brownScoring () => {
+    function brownScoring(userScore, brownScore) {userScore + brownScore};
+};
+green.addEventListener("click",greenScoring () => {
+    function greenScoring(userScore, greenScore) {userScore + greenScore};
+};
+red.addEventListener("click",redScoring () => {
+    function redScoring(userScore, redScore) {userScore + redScore};
+};
 const userScore = (0);
 const blueScore = (1);
 const brownScore = (2);
