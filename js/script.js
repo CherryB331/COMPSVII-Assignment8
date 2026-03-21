4782793e4f168da3fc63a380a21fc0ea04695ea6
CherryB331 <brianna.cherry@salem.edu>
2026-03-21 16:31:38 -0400

    ccj#   (use "git push" to publish your local commits)

--- js/script.js
+++ js/script.js
@@ -79,6 +79,7 @@ function getResult() {
   }
   return result;
 }
+let highest = 0;
 let result = getResult();
 let colorEnd = document.getElementById("#result-container");
 function showColorResult() {
