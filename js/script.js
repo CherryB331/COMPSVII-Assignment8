0902cc3d69a89e8fc1c04c477ee853579e574b98
CherryB331 <brianna.cherry@salem.edu>
2026-03-21 17:08:11 -0400

    /khv

--- js/script.js
+++ js/script.js
@@ -53,6 +53,9 @@ queFor.forEach(forbutton => {
 });
 
 function getResult() {
+let counter = {};
+scores.forEach(type => {counter[type] = (counter[type] || 0) + 1;});
+
   let highest = 0;
   let result = "";
 
